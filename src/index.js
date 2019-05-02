5aa1704a0a7659216508878b5d2e76f360cb1c62
Walker Randolph Smith <walkerrandolphsmith@users.noreply.github.com>
2019-05-01 22:12:42 -0400

    Update index.js
    ensure requests to various services don't wait for each other synchronously.

--- src/index.js
+++ src/index.js
@@ -30,29 +30,14 @@ const deafultSecrets = {
 
 const chronologicalSorter = (x, y) => -1 * moment.utc(x.date).diff(moment.utc(y.date));
 
-const applyEvenOddProperty = (post, i) => ({
-    ...post,
-    direction: i % 2 === 0 ? 'direction-r' : 'direction-l'
-});
-
 export default async (availableServices, secrets=deafultSecrets) => {
-    const instagramStream = await availableServices.instagram
-        ? getInstagramStream({ ...secrets.instagram }) : [];
+    const fns = [];
+    if (availableServices.instagram) fns.push(() => getInstagramStream({ ...secrets.instagram }));
+    if (availableServices.facebook) fns.push(() => getFacebookStream({ ...secrets.facebook }));
+    if (availableServices.youTube) fns.push(() => getYoutubeStream({ ...secrets.youtube, ... secrets.googleServiceAccount }));
+    if (availableServices.twitter) fns.push(() => getTwitterStream({ ...secrets.twitter }));
+    
+    const posts = await Promise.all(fns.map(fn => fn()));
 
-    const facebookStream = await availableServices.facebook
-        ? getFacebookStream({ ...secrets.facebook }) : [];
-
-    const youTubeStream = await availableServices.youTube
-        ? getYoutubeStream({ ...secrets.youtube, ... secrets.googleServiceAccount }) : [];
-
-    const twitterStream = await availableServices.twitter
-        ? getTwitterStream({ ...secrets.twitter }) : [];
-
-    return []
-        .concat(facebookStream)
-        .concat(instagramStream)
-        .concat(twitterStream)
-        .concat(youTubeStream)
-        .sort(chronologicalSorter)
-        .map(applyEvenOddProperty);
+    return posts.sort(chronologicalSorter);
 };
